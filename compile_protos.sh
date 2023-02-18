ec68f25bdf60753e6b9797e6522e60775c073c7d
Michael Berkowitz <michael.berkowitz@gmail.com>
2023-02-18 11:42:27 -0500

    Correctly generate stubs as well

--- compile_protos.sh
+++ compile_protos.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 cp ../prefab-cloud/prefab.proto .
-protoc -I=. --python_out=. ./prefab.proto
-
+python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. ./prefab.proto

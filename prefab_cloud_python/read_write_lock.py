558e4ace219f08f234be9ab742ab488ad26ae220
James Kebinger <james.kebinger@prefab.cloud>
2024-01-25 15:41:37 -0600

    Fix read-write-lock and lock timeout handling

--- prefab_cloud_python/read_write_lock.py
+++ prefab_cloud_python/read_write_lock.py
@@ -12,10 +12,21 @@ class ReadWriteLock:
         self._readers = 0
         self._write_locked = False
 
-    def acquire_read(self, timeout=1):
+    def try_acquire_read(self, timeout):
+        acquired = self._read_ready.acquire(blocking=True, timeout=timeout)
+        if not acquired:
+            print("not acquired, returning False")
+            return False
+        self._acquire_read_core()
+        return True
+
+    def acquire_read(self):
         """Acquire a read lock. Blocks only if a thread has
         acquired the write lock."""
-        self._read_ready.acquire(timeout)
+        self._read_ready.acquire(blocking=True)
+        self._acquire_read_core()
+
+    def _acquire_read_core(self):
         try:
             self._readers += 1
         finally:
@@ -23,7 +34,7 @@ class ReadWriteLock:
 
     def release_read(self):
         """Release a read lock."""
-        self._read_ready.acquire()
+        self._read_ready.acquire(blocking=True)
         try:
             self._readers -= 1
             if not self._readers:
@@ -40,10 +51,36 @@ class ReadWriteLock:
         finally:
             self.release_read()
 
+    @contextmanager
+    def read_locked_timeout(self, timeout=1):
+        """This method is designed to be used via the `with` statement.
+            check return value: returns True only if lock is acquired
+        """
+        result = self.try_acquire_read(timeout=timeout)
+        try:
+            if result:
+                yield result
+            else:
+                #return the result even if false rather than throw an exception, caller needs to check the result
+                yield result
+
+        finally:
+            if result:
+                self.release_read()
+
+    @contextmanager
+    def try_read_locked(self, timeout=10):
+        """This method is designed to be used via the `with` statement."""
+        try:
+            self.acquire_read()
+            yield
+        finally:
+            self.release_read()
+
     def acquire_write(self):
         """Acquire a write lock. Blocks until there are no
         acquired read or write locks."""
-        self._read_ready.acquire()
+        self._read_ready.acquire(blocking=True)
         while self._readers > 0:
             self._read_ready.wait()
         self._write_locked = True
@@ -52,3 +89,4 @@ class ReadWriteLock:
         """Release a write lock."""
         self._read_ready.release()
         self._write_locked = False
+

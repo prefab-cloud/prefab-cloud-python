ec6e4ff979fde8b82335d6041dd343f75addf38c
James Kebinger <james.kebinger@prefab.cloud>
2024-01-26 16:32:11 -0600

    its not a tuple

--- prefab_cloud_python/_telemetry.py
+++ prefab_cloud_python/_telemetry.py
@@ -232,12 +232,8 @@ class EvaluationRollup(object):
         if evaluation.config:
             reportable_value = None
             try:
-                reportable_value = (
-                    HashableProtobufWrapper(
-                        evaluation.deepest_value().reportable_wrapped_value().value
-                    ),
-                )
-            except:
+                reportable_value = HashableProtobufWrapper(evaluation.deepest_value().reportable_wrapped_value().value)
+            except Exception:
                 pass
             self.counts[
                 (

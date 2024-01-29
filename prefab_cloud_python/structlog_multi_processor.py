6aedf9934fc32a1f4b3edd9604e26112c47697f7
James Kebinger <james.kebinger@prefab.cloud>
2024-01-29 10:34:52 -0600

    document the StructLogMultiProcessor

--- prefab_cloud_python/structlog_multi_processor.py
+++ prefab_cloud_python/structlog_multi_processor.py
@@ -1,4 +1,9 @@
 class StructlogMultiProcessor:
+    """
+    This is a utility to wrap the three prefab structlog processors as just one in your structlog configuration
+    It can be instantiated by the create_prefab_structlog_processor method exposed by the prefab_cloud_python module
+    use of this is preferred over the individual processors from _structlog_processors.py
+    """
     def __init__(self, processors):
         self.processors = processors
 

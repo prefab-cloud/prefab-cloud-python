edf5fca7b0ea3c88b1bad8785d2a6a9f04828aa9
James Kebinger <james.kebinger@prefab.cloud>
2025-02-24 11:08:51 -0600

    add dict to ConfigValueType's union (#118)

--- prefab_cloud_python/constants.py
+++ prefab_cloud_python/constants.py
@@ -2,5 +2,5 @@ from typing import Optional, Union
 from datetime import timedelta
 
 NoDefaultProvided = object()
-ConfigValueType = Optional[Union[int, float, bool, str, list[str], timedelta]]
+ConfigValueType = Optional[Union[int, float, bool, str, list[str], timedelta, dict]]
 ContextDictType = dict[str, dict[str, ConfigValueType]]
